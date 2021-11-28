debe787363e683f6f00f15013c6050b91aa5a922
Gustavo Velasquez <gvelasquez@consultoriaglobal.com.ar>
2021-11-28 14:25:11 -0300

    Init project, add controllers and valitation Models

---
+++ ImprovedWorkCenter/Context/ImprovedWorkCenterContext.cs
@@ -0,0 +1,24 @@
+﻿using ImprovedWorkCenter.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImprovedWorkCenter.Context
+{
+    public class ImprovedWorkCenterContext : DbContext
+    {
+        public ImprovedWorkCenterContext(DbContextOptions<ImprovedWorkCenterContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Club> Clubs { get; set; }
+
+        public DbSet<Actividad> Socios { get; set; }
+        public DbSet<Plan> Planes { get; set; }
+        public DbSet<Socio> Actividades { get; set; }
+        public DbSet<ActividadSocio> ActividadSocios { get; set; }
+
+    }
+}
